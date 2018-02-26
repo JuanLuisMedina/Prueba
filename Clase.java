56104f637233a2db08b4b717e85a9acff24a548e
Juan Luis <juanluismedina1995@gmail.com>
2018-02-26 14:00:47 +0100

    Añadido clase

---
+++ Clase.java
@@ -0,0 +1,29 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class Clase
+{
+     
+    private int x;
+
+    /**
+     * Constructor  
+     */
+    public Clase()
+    {
+         x = 5;
+    }
+
+    /**
+     *  
+     */
+    public int metodo()
+    {
+         int resultado = x + 4;
+         return resultado;
+    }
+}
